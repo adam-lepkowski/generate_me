85e4ffa2149b6accb5db9c81e8855490845fe816
adam <adam.lepkowski@hotmail.com>
2022-12-07 21:30:51 +0100

    Replace index method with IndexView

--- app/views.py
+++ app/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from django.views import View
 
 from .forms import GeneratorForm
 
 # Create your views here.
 
 
-def index(request):
+
+class IndexView(View):
     """
     Return main page.
     """
+
+    def get(self, request):
+        context = {
+            "form": GeneratorForm(label_suffix="")
+        }
+        return render(request, "app/index.html", context)
     
-    context = {
-        "form": GeneratorForm(label_suffix="")
-    }
-    return render(request, "app/index.html", context)
+    def post(self, request):
+        context = {
+            "form": GeneratorForm(label_suffix="")
+        }
+        return render(request, "app/index.html", context)
