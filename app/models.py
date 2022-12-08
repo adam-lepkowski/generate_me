04f3f1607355376cde9dbf7b4363eed2f2f6fa88
adam <adam.lepkowski@hotmail.com>
2022-12-08 22:04:53 +0100

    Add LastNameModel, FirstNameModel

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class FirstNameModel(models.Model):
+    """
+    First name with assigned gender.
+    """
+
+    first_name = models.CharField(max_length=40)
+    gender = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"{self.first_name}: {self.gender}"
+
+
+class LastNameModel(models.Model):
+    """
+    Last name with assigned gender.
+    """
+
+    last_name = models.CharField(max_length=40)
+    gender = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"{self.last_name}: {self.gender}"
