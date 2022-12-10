871abecaa825795912c08c91bbbfa67c236bf3b6
adam <adam.lepkowski@hotmail.com>
2022-12-10 22:17:06 +0100

    Listen to icon hover events

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -4,5 +4,17 @@ for (i=0; i < icons.length; i++) {
     icons[i].addEventListener("click", function() {
         var icon = this;
         navigator.clipboard.writeText(icon.parentElement.children[1].value);
+        icon.parentElement.children[3].innerText = "Copied!"
     })
+    
+    icons[i].addEventListener("mouseover", function(){
+        var icon = this;
+        icon.parentElement.children[3].style.visibility = "visible";
+    })
+
+    icons[i].addEventListener("mouseout", function(){
+        var icon = this;
+        icon.parentElement.children[3].style.visibility = "hidden";
+        icon.parentElement.children[3].innerText = "Copy";
+    }) 
 }
