c724854d5f1a5519d97396e26923744f2712913e
adam <adam.lepkowski@hotmail.com>
2022-12-10 08:54:15 +0100

    Move draw_identity to utils

--- app/views.py
+++ app/views.py
@@ -1,38 +1,8 @@
-import random
-
 from django.shortcuts import render
 from django.views import View
 
 from .forms import GeneratorForm
-from .models import FirstNameModel, LastNameModel
-
-
-def draw_identity():
-    """
-    Draw a random identity from FirstNameModel and LastNameModel.
-
-    Returns
-    ----------
-    dict
-        a random identity
-    """
-
-    min_val = 1
-    max_fname = FirstNameModel.objects.order_by("-id")[0].id
-    fname_id = random.randint(min_val, max_fname)
-    fname = FirstNameModel.objects.get(id=fname_id)
-    lname_ids = [
-        l.id for l in LastNameModel.objects.filter(gender=fname.gender)]
-    lname_id = random.choice(lname_ids)
-    lname = LastNameModel.objects.get(id=lname_id)
-    result = {
-        "first_name": fname.first_name,
-        "last_name": lname.last_name,
-        "gender": fname.gender
-    }
-
-    return result
-
+from .utils import draw_identity
 
 class IndexView(View):
     """
