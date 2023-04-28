5b69ea0d5ecc9841f24cef0fb7426dca49e2383a
adam <adam.lepkowski@hotmail.com>
2023-04-28 21:38:15 +0200

    Remove IDENTITY

--- app/tests/test_index.py
+++ app/tests/test_index.py
@@ -5,15 +5,6 @@ from django.test import TestCase
 from app.views import IndexView
 
 
-IDENTITY = {
-    "first_name": "foo",
-    "last_name": "bar",
-    "gender": "male",
-    "dob": "2020-10-10",
-    "nickname": "foo.bar"
-}
-
-
 class TestIndex(TestCase):
 
     def test_index_get(self):
