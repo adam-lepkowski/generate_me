f5d772fdee3e9777d857bf7f0e96004c8eae2b6f
adam <adam.lepkowski@hotmail.com>
2023-04-28 20:31:44 +0200

    Add fetchIdentity

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -18,3 +18,29 @@ for (i=0; i < icons.length; i++) {
         icon.parentElement.children[3].innerText = "Copy";
     }) 
 }
+
+/**
+ * Fetch randomly generated identity json. Insert result in appropriate form
+ * fields.
+ */
+async function fetchIdentity() {
+    var url = document.querySelector("#identity-form").getAttribute("identity-url");
+    response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "X-Requested-With": "XMLHttpRequest"
+        }
+    });
+    var data = await response.json();
+    document.querySelector("#id_first_name").value = data.first_name
+    document.querySelector("#id_last_name").value = data.last_name
+    document.querySelector("#id_dob").value = data.dob
+    document.querySelector("#id_nickname").value = data.nickname
+    if (data.gender == "male") {
+        document.querySelector(".fa-mars").classList.add("icon-male")
+        document.querySelector(".fa-venus").classList.remove("icon-female")
+    } else {
+        document.querySelector(".fa-mars").classList.remove("icon-male")
+        document.querySelector(".fa-venus").classList.add("icon-female")
+    }
+}
